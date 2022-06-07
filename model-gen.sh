46993b4a15640635bc8adcdf5d2b30ef70d96153
Katy Dickenson <katherine.dickenson@lmco.com>
2022-06-07 16:02:24 +0000

    Updated variable usage for efficiency. Tests for possible server errors

--- model-gen.sh
+++ model-gen.sh
@@ -1,20 +1,20 @@
-curl -L -o cyclonedx_dwnld.tar.gz https://github.com/CycloneDX/specification/archive/refs/tags/1.4.tar.gz -vvv
-tar -xvf cyclonedx_dwnld.tar.gz
+#!/bin/bash
+answer=$1
 
-#mkdir cyclonedx_types
-read -p "Which version of CycloneDX would you like to make a pydantic model of?(Answer with _._ format - EX: 1.4): " answer
+HTTP_CODE=$(curl -s -w "%{http_code}\n" -o /dev/null https://github.com/CycloneDX/specification/releases/tag/$answer -vvv)
 
-if [[ $answer =~ "1.4" ]]; then
-    datamodel-codegen  --input ./specification-1.4/schema/bom-1.4.schema.json --input-file-type jsonschema --output ./pydantic1_4
-    sed -i s/unique_items=True/"'REMOVED LM unique_items=True'"/g ./pydantic1_4/__init__.py
-elif [[ $answer =~ "1.3" ]]; then
-    datamodel-codegen  --input ./specification-1.4/schema/bom-1.3.schema.json --input-file-type jsonschema --output ./pydantic1_3
-    sed -i s/unique_items=True/"'REMOVED LM unique_items=True'"/g ./pydantic1_3/__init__.py
-elif [[ $answer =~ "1.2" ]]; then
-    datamodel-codegen  --input ./specification-1.4/schema/bom-1.2.schema.json --input-file-type jsonschema --output ./pydantic1_2
-    sed -i s/unique_items=True/"'REMOVED LM unique_items=True'"/g ./pydantic1_2/__init__.py
-else
-    echo "That is not a current version."  
+if [ $HTTP_CODE != "404" ]; then
+    curl -L -o cyclonedx_dwnld.tar.gz https://github.com/CycloneDX/specification/archive/refs/tags/$answer.tar.gz -vvv
+    tar -xvf cyclonedx_dwnld.tar.gz 
+
+    py_file=pydantic$answer
+    py_file=${py_file//./_} 
+
+    datamodel-codegen  --input ./specification-$answer/schema/bom-$answer.schema.json --input-file-type jsonschema --output ./$py_file
+    sed -i s/unique_items=True/"'REMOVED LM unique_items=True'"/g ./$py_file/__init__.py
+else 
+    echo "The version selected does not exist."
 fi
+
 
 

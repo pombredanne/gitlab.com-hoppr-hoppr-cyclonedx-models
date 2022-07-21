ae23d735fe08ffb53858abd695b029f21f5e3d81
lmco.kdickenson <katherine.dickenson@lmco.com>
2022-07-21 18:04:46 +0000

    Fix: Model generation

--- model-gen.sh
+++ model-gen.sh
@@ -12,7 +12,7 @@ if [ $HTTP_CODE != "404" ]; then
     py_file=cyclonedx_$answer
     py_file=${py_file//./_} 
 
-    poetry run python -m datamodel-codegen --input ./specification-$answer/schema/bom-$answer.schema.json --input-file-type jsonschema --output ./hoppr_cyclonedx_models/$py_file
+    poetry run datamodel-codegen --input ./specification-$answer/schema/bom-$answer.schema.json --input-file-type jsonschema --output ./hoppr_cyclonedx_models/$py_file
     sed -i s/unique_items=True/"'REMOVED LM unique_items=True'"/g ./hoppr_cyclonedx_models/$py_file/__init__.py
 else 
     echo "The version selected does not exist. Example Usage: ./model-gen.sh 1.4"
